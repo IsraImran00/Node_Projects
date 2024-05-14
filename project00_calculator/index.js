4868c02bf2f6667ab092812b50ba8ed0772568f3
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-05-14 20:33:38 +0500

    Add files via upload

---
+++ project00_calculator/index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    { message: "Enter firstNumber", type: "number", name: "firstNumber" },
+    { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+    { message: "Select one Operator to perform Operations", type: "list", name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// Conditional Statment
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
+;
